74d46044bf6bc206f0bb6a3bb9dac2f090b7dc80
Zain <117276807+ZainMVP@users.noreply.github.com>
2022-12-21 16:14:28 +0530

    ...

---
+++ CRUDAPI/CRUDAPI/Controller/MembersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using static CRUDAPI.Entity.Members;
+
+namespace CRUDAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private IConfiguration Configuration;
+        
+
+        public MembersController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetMember")]
+
+        public MemberResponse GetMember()
+        {
+            MemberResponse memberResponse = new MemberResponse();
+
+            return memberResponse;
+        }
+    }
+}
---
+++ CRUDAPI/CRUDAPI/Entity/Members.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAPI.Entity
+{
+    public class Members
+    {
+        public class MemberRequest
+        {
+            private string name;
+            private int age;
+            private string address;
+
+            public string memberName
+            {
+                get{ return name;}
+                set { name = value; }
+            }
+            public int memberAge 
+            {
+                get { return age; }
+                set{ age = value; }
+            }
+            public string memberAddress
+            {
+                get { return address; }
+                set { address = value; }
+            }
+        }
+        public class MemberResponse
+        {
+            private Boolean success;
+            private string message;
+
+            public Boolean isSucess
+            {
+                get { return success; }
+                set { success = value; }
+            }
+            public string isMessage
+            {
+                get { return message; }
+                set { message = value; }
+            }
+        }
+    }
+}
